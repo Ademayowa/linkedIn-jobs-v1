20252061f05f4a855492b77d3c4a1a01da90e2e9
Ademayowa <ademayowaadeniyi@gmail.com>
2022-04-07 16:40:10 +0100

    changed favicon

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,7 @@ import {
 } from 'react-bootstrap';
 import Link from 'next/link';
 import Image from 'next/image';
+import { BsSearch } from 'react-icons/bs';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
@@ -16,9 +17,7 @@ export default function Header() {
     <Navbar collapseOnSelect expand='md' className='bg-white shadow-sm'>
       <Container>
         <Navbar.Brand>
-          <Link href='/'>
-            <Image src='https://rb.gy/dpmd9s' width={55} height={55} />
-          </Link>
+          <Image src='https://rb.gy/dpmd9s' width={55} height={55} />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
